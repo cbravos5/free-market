296706fdf3bdec45be67388a2a0644fb11df0208
cbravos <cbravos@pop-os.localdomain>
2023-03-12 18:56:32 -0300

    feat: add schema

--- src/gql/typeDefs.ts
+++ src/gql/typeDefs.ts
@@ -1,3 +1,33 @@
 import { gql } from 'apollo-server-express';
 
-export const typeDefs = gql``;
+export const typeDefs = gql`
+    type Business {
+        name: String!
+        location: Point!
+        products: [Product!]! @relationship(type: "OWNS", direction: OUT)
+        distributors: [Distributor!]! @relationship(type: "ROUTE", direction: OUT)
+    }
+
+    type Product {
+        name: String!
+        price: Float!
+        businesses: [Business!]! @relationship(type: "OWNS", direction: IN)
+        users: [User!]! @relationship(type: "BUYS", direction: IN)
+    }
+
+    type User {
+        name: String!
+        location: Point!
+        purchases: [Product!]! @relationship(type: "BUYS", direction: OUT)
+        distributors: [Distributor!]! @relationship(type: "ROUTE", direction: IN)
+    }
+
+    type Distributor {
+        name: String!
+        location: Point!
+        businesses: [Business!]! @relationship(type: "ROUTE", direction: IN)
+        deliversToDistributor: [Distributor!]! @relationship(type: "ROUTE", direction: IN)
+        distributorsDeliversTo: [Distributor!]! @relationship(type: "ROUTE", direction: OUT)
+        users: [User!]! @relationship(type: "ROUTE", direction: OUT)
+    }
+`;
